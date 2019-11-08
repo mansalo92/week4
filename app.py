831f1f6bf33767f7a65bdc0f89d9620f47a8c8fe
mansalo92 <56358788+mansalo92@users.noreply.github.com>
2019-11-08 17:47:57 -0500

    replace

--- app.py
+++ app.py
@@ -202,6 +202,7 @@ def update_indicators(exchange, leverage, start_date, end_date):
 
 
 
-if __name__ == "__main__": app.run_server(debug=True)
+if __name__ == '__main__':
+app.run_server(debug=True, host= '0.0.0.0')
 
                
