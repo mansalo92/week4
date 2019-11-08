0b0bfb8411554513db7cc495ed0ce7ad3ba7547e
mansalo92 <56358788+mansalo92@users.noreply.github.com>
2019-11-08 18:02:10 -0500

    ojala

--- app.py
+++ app.py
@@ -203,6 +203,6 @@ def update_indicators(exchange, leverage, start_date, end_date):
 
 
 if __name__ == '__main__':
-app.run_server(debug=True, host= '0.0.0.0')
+    app.run_server(debug=True, host= '0.0.0.0')
 
                
